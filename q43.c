7e8a62d86d50a765b7501f0677395481d907f5ce
OM-6368 <omadlakha80@gmail.com>
2025-11-06 19:55:41 +0530

    .

--- q43.c
+++ q43.c
@@ -12,3 +12,27 @@ Output 2:
 Not strong number
 */
 #include <stdio.h>
+int main(void) {
+    int matrix[3][3];
+    int i, j;
+    printf("Enter elements of 3x3 matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("Enter element [%d][%d]: ", i, j);
+            if (scanf("%d", &matrix[i][j]) != 1) {
+                fprintf(stderr, "Invalid input\n");
+                return 1;
+            }
+        }
+    }
+
+    printf("\nMatrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
