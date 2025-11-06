6031c663bfc86b94aed66687922eae044485c8e2
OM-6368 <omadlakha80@gmail.com>
2025-11-06 19:58:54 +0530

    DAY_22

--- q44.c
+++ q44.c
@@ -14,3 +14,23 @@ Output 2:
 Approximate sum: 4.4
 
 */
+#include <stdio.h>
+int main(void) {
+    int n, i;
+    double sum = 0.0;
+
+    printf("Enter the number of terms: ");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+
+    for (i = 0; i < n; i++) {
+        int numerator = 2 * i + 1;
+        int denominator = 2 * i + 2;
+        sum += (double)numerator / denominator;
+    }
+
+    printf("Approximate sum: %.1f\n", sum);
+    return 0;
+}
