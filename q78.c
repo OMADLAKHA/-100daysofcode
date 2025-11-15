37eb2a661bc5cf41abf456a9a25aec273abcb038
OM-6368 <omadlakha80@gmail.com>
2025-11-15 14:27:09 +0530

    DAY_39

---
+++ q78.c
@@ -0,0 +1,42 @@
+/*
+Q78: Find the sum of main diagonal elements for a square matrix.
+
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 1:
+15
+
+*/
+#include <stdio.h>
+#define MAX_SIZE 100
+int main() {
+    int matrix[MAX_SIZE][MAX_SIZE];
+    int rows, cols;
+    int sum = 0;
+
+    // Read the number of rows and columns
+    scanf("%d %d", &rows, &cols);
+
+    // Read the matrix elements
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Calculate the sum of main diagonal elements
+    if (rows == cols) {
+        for (int i = 0; i < rows; i++) {
+            sum += matrix[i][i];
+        }
+    }
+
+    // Print the sum
+    printf("%d\n", sum);
+
+    return 0;
+}
