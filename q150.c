eafac0b8ad23aa2da5ac7c5a38b1ba1cb1431d0f
OMADLAKHA <omadlakha80@gmail.com>
2025-11-29 00:01:50 +0530

    Add files via upload

--- q150.c
+++ q150.c
@@ -1,4 +1,4 @@
-/* Q150: Use pointer to struct to modify and display data using -> operator.
+/*Q150: Use pointer to struct to modify and display data using -> operator.
 
 Sample Test Cases:
 Input 1:
@@ -8,24 +8,17 @@ Modified Data: Name: John | Roll: 106 | Marks: 91
 
 */
 #include <stdio.h>
-#include <string.h>
-
-typedef struct {
-    char name[50];
-    int roll;
-    int marks;
-} Student;
-
-int main() {
-    Student s;
-    Student *ptr = &s; 
-
-    strcpy(ptr->name, "John");
-    ptr->roll = 106;
-    ptr->marks = 91;
-
-    printf("Modified Data: ");
-    printf("Name: %s | Roll: %d | Marks: %d\n", ptr->name, ptr->roll, ptr->marks);
-
+int main () {
+    struct Student {
+        char name[50];
+        int roll;
+        int marks;
+    };
+    struct Student student1;
+    struct Student *ptr;
+    ptr = &student1;
+    printf("Enter Name, Roll and Marks: ");
+    scanf("%s %d %d", ptr->name, &ptr->roll, &ptr->marks);
+    printf("Modified Data: Name: %s | Roll: %d | Marks: %d\n", ptr->name, ptr->roll, ptr->marks);
     return 0;
 }
